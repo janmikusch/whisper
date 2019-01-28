7437dbcfcccd57ddd426d779aae302e4247261ce
robinunger <unger.robin@hotmail.com>
2019-01-28 17:39:04 +0100

    add displace to attack animation

--- source/HeroAnimationComponent.cpp
+++ source/HeroAnimationComponent.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "window.h"
 #include "TextureManager.h"
+#include "CharacterMoveComponent.h"
 
 HeroAnimationComponent::HeroAnimationComponent(std::shared_ptr<GameObject> parent, Layer layer, float animationTime) :
 	AnimationComponent(parent, layer, animationTime)
@@ -14,7 +15,12 @@ void HeroAnimationComponent::update( const float fDeltaTimeSeconds)
 {
 	m_frameTime = m_frameClock.restart();
 	
-	m_animatedSprite.setPosition(m_parent->getPosition());
+
+	if (m_parent->getComponent<CharacterMoveComponent>()->getState() == 2)
+		m_animatedSprite.setPosition(m_parent->getPosition() + m_diplace);
+	else
+		m_animatedSprite.setPosition(m_parent->getPosition());
+
 	m_animatedSprite.setRotation(m_parent->getRotation());
 	m_animatedSprite.setOrigin(m_parent->getOrigin());
 
