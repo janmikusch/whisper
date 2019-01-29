692a3979d29dd35a04c672d9e1c711586c28e61e
Jan Mikusch <jmikusch.mmt-b2017@fh-salzburg.ac.at>
2019-01-29 12:12:20 +0100

    create Menu UI with Credits

--- source/MenuState.cpp
+++ source/MenuState.cpp
@@ -12,6 +12,7 @@
 #include "GameObjectManager.h"
 #include "window.h"
 #include "FontManager.h"
+#include "GUI.h"
 
 MenuState::MenuState(StateType type):State(type)
 {
@@ -22,10 +23,6 @@ State::StateType MenuState::update(const float fDeltaTimeSeconds)
 	GameObjectManager& objManager = GameObjectManager::getInstance();
 	InputManager& inputManager = InputManager::getInstance();
 
-	if (inputManager.isKeyDown("StartGame", 0))
-	{
-		return StateType::STATE_GAMEPLAY;
-	}
 
 	for (std::shared_ptr<GameObject> o : objManager.getList())
 	{
@@ -50,18 +47,7 @@ void MenuState::init()
 {
 	GameObjectManager& objManager = GameObjectManager::getInstance();
 
-	sf::Text text = sf::Text("Press SPACE to start", FontManager::getInstance().getFont("arial"));
-	text.setCharacterSize(30);
-	text.setStyle(sf::Text::Bold);
-	text.setFillColor(sf::Color::White);
-
-	auto window = engine::Window::getInstance().getWindow();
-	auto position = sf::Vector2f(window->getSize().x / 2 - text.getString().getSize()*text.getCharacterSize() / 4, window->getSize().y / 2);
-	text.setPosition(position);
-	std::shared_ptr<GameObject> uiObject = std::make_shared<GameObject>(position, "menu");
-	uiObject->addComponent(std::make_shared <TextGuiComponent>(uiObject, Layer::FOREGROUND1, text));
-
-	objManager.add(uiObject);
+	engine::GUI::getInstance().init(m_type);
 
 	objManager.applyChanges();
 
