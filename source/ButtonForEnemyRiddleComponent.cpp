3b312a01f3cbbcb044cad96e2e79cdc285f76347
robinunger <unger.robin@hotmail.com>
2019-01-31 16:11:11 +0100

    spawn enemies to different times

--- source/ButtonForEnemyRiddleComponent.cpp
+++ source/ButtonForEnemyRiddleComponent.cpp
@@ -27,11 +27,18 @@ void ButtonForEnemyRiddleComponent::onNotify(const GameObject& collidedWith, std
 
 	if (cge != nullptr && cge->type == engine::CollisionGameEvent::CollisionType::ENTER)
 	{
+		std::vector<std::shared_ptr<GameObject>> emptyvec;
+		m_enemies.swap(emptyvec);
+
 		for (auto o : RoomManager::getInstance().getCurrentRoomObjects())
 		{
 			if (o->getName() == "enemy")
-				o->getComponent<EnemyMoveComponent>()->setStandingAnimation();
+				m_enemies.push_back(o);
 		}
+
+		std::random_shuffle(m_enemies.begin(), m_enemies.end());
+
+		nextEnemy = m_enemies.begin();
 	}
 	if (cge != nullptr && cge->type == engine::CollisionGameEvent::CollisionType::EXIT)
 	{
@@ -41,4 +48,27 @@ void ButtonForEnemyRiddleComponent::onNotify(const GameObject& collidedWith, std
 				o->getComponent<EnemyMoveComponent>()->setFightingState(true);
 		}
 	}
+}
+
+void ButtonForEnemyRiddleComponent::update(const float fDeltaTimeSeconds)
+{
+	ButtonComponent::update(fDeltaTimeSeconds);
+
+	if (m_enemies.empty())
+		return;
+
+	if(nextEnemy != m_enemies.end())
+		m_timer += fDeltaTimeSeconds;
+
+	if (m_timer > 1.0f && nextEnemy != m_enemies.end())
+	{
+		(*nextEnemy)->getComponent<EnemyMoveComponent>()->setStandingAnimation();
+		m_timer = 0;
+		nextEnemy++;
+	}
+}
+
+void ButtonForEnemyRiddleComponent::init()
+{
+	m_timer = 0.0f;
 }
